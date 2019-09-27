69b17ec283de03dab60ac6ea29260a98b333fedd
Jorden <jorden@jordens-mbp.home>
2019-09-27 10:48:46 +0100

    worked on click dice function. if dice roll is different to 1 then add to roundscore and display roundscore else change active player, reset roundscore, and toggle 'active' clclass for user

--- app.js
+++ app.js
@@ -9,17 +9,50 @@ GAME RULES:
 
 */
 
-var scores, roundScores, activePlayer, dice;
+var scores, roundScores, activePlayer;
 
 scores = [0, 0];
 roundScores = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
-
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
-
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+    //1. Random number generator
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    //2. Display the results
+    var diceDom = document.querySelector('.dice');
+    diceDom.style.display = 'block';
+    diceDom.src = 'dice-' + dice + '.png';
+    //3. Update the round number IF the number is NOT 1
+    if (dice !== 1) {
+        //add score
+        roundScores += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScores;
+    } else {
+        //next players turn
+        activePlayer === 0 ? activePlayer = 1: activePlayer = 0;
+        roundScores = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        document.querySelector('.dice').style.display = "none";
+
+
+    }
+
+})
+
+
+// var x = document.querySelector('#score-0').textContent;
+// console.log(x);
