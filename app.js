bb0493d9d6b73e0f24365a9ccecad1eb8f5f81f5
Jorden <jorden@jordens-mbp.home>
2019-09-27 11:39:10 +0100

    added a check if player won the game within btn hold event

--- app.js
+++ app.js
@@ -48,9 +48,15 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
     // update ui
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
     // check if player won game
-
-    //next player
-    nextPlayer();
+    if (scores[activePlayer] >= 20) {
+        document.querySelector('#name-' + activePlayer).textContent= "Winner!";
+        document.querySelector('.dice').style.display = "none";
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    } else {
+        //next player
+        nextPlayer();
+    }
 
 });
 
