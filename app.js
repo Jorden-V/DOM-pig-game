7e517bca4549f9b5e8056500d857abf05a1f24ab
Jorden <jorden@jordens-mbp.home>
2019-09-27 09:22:37 +0100

    used document.queryselector to display dice roll on active player, store score 0 in var x and hide dice class

--- app.js
+++ app.js
@@ -17,4 +17,9 @@ activePlayer = 0;
 
 dice = Math.floor(Math.random() * 6) + 1;
 
+document.querySelector('#current-' + activePlayer).textContent = dice;
 
+var x = document.querySelector('#score-0').textContent;
+console.log(x);
+
+document.querySelector('.dice').style.display = 'none';
