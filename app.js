4ebe7c233683c6e01e129f409321fdbe13ba95e0
Jorden <jorden@jordens-mbp.home>
2019-09-27 08:59:17 +0100

    setup dice variable to display random number between 1 and 6

--- app.js
+++ app.js
@@ -9,9 +9,12 @@ GAME RULES:
 
 */
 
-var scores, roundScores, activePlayer;
+var scores, roundScores, activePlayer, dice;
 
 scores = [0, 0];
 roundScores = 0;
 activePlayer = 0;
+
+dice = Math.floor(Math.random() * 6) + 1;
+
 
